d0390d51c3beee7e549bf24ef462909eb7287e5c
Marco Sievers de Almeida <msa@lionx.com.br>
2022-10-11 17:58:09 -0300

    feat: add ouroinvest term

--- func/src/domain/enums/file/term_file.py
+++ func/src/domain/enums/file/term_file.py
@@ -25,5 +25,6 @@ class UserTerms(Enum):
         "term_customer_relationship_summary_dl_us"
     )
     TERM_ALL_AGREEMENT_GRINGO_DL = "term_all_agreement_gringo_dl"
+    TERM_OUROINVEST = "term_ouroinvest"
     TERM_GRINGO_WORLD = "term_gringo_world"
     TERM_GRINGO_WORLD_GENERAL_ADVICES = "term_gringo_world_general_advices"
